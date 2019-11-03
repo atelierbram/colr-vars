90ec9a5139219a88c031d91093cf8ccb544c6c39
atelierbram <bramdeh@gmail.com>
2019-11-03 16:23:23 +0100

    edit

--- gruntfile.js
+++ gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
      options: {
        map: true,
        processors: [
-         require("autoprefixer")({ browsers: ["last 2 versions"] }),
+         require("autoprefixer")(),
          require('cssnano')(),
        ]
      },
